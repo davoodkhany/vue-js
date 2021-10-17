5d8af0ad923a1306e5a6cff71a52447cb4a39228
davood <davoodkhany@gmail.com>
2021-10-17 06:50:54 +0330

    Merge branch 'master' of https://github.com/davoodkhany/vue-js

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,46 @@
 
 
 <template>
+<<<<<<< HEAD
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Alert/>
+</template>
+
+<script>
+import Alert from './components/Alert'
+
+export default {
+  name: 'App',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Alert
+=======
   <img alt="Vue logo" src="./assets/logo.png" />
-  <alert></alert>
+  <h2>{{message}}</h2>
 </template>
 
 
 
 <script>
-import alert from './components/Alert'
-
-export default {
-  components: {
-    alert,
-  },
-};
+  export default{
+      data (){
+      return {
+        message:'davood'
+      }
+    }
+>>>>>>> 440b9a1a388e8d67f3c333337c95b4d097e7ee6a
+  }
 </script>
 
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
