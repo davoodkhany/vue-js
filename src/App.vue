8afa9691d3002ea9800637abdc7222346dd746f3
davood <davoodkhany@gmail.com>
2021-10-22 23:48:39 +0330

    todoapps cpmplate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <NavBar></NavBar>
-  <div class="bg-secondary p-5">
+  <div class="p-5 bg-secondary">
     <h1>Welcome</h1>
     <p>lorem ipsum dolor sit amet</p>
     <div class="form-group d-flex justify-content-center">
@@ -9,8 +9,8 @@
     </div>
   </div>
 
-  <div class="d-flex justify-content-center mt-5">
-    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <div class="mt-5 d-flex justify-content-center">
+    <ul class="mb-3 nav nav-pills" id="pills-tab" role="tablist">
       <li class="nav-item" role="presentation">
         <button
           class="nav-link active"
@@ -42,22 +42,16 @@
     </ul>
   </div>
   <div
-    class="
-      tab-content
-      flex-column
-      justify-content-center
-      d-flex
-      align-items-center
-    "
+    class=" tab-content flex-column justify-content-center d-flex align-items-center"
     id="pills-tabContent"
   >
     <div
-      class="tab-pane fade show active col-4 mb-2"
+      class="mb-2 tab-pane fade show active col-4"
       id="pills-home"
       role="tabpanel"
       aria-labelledby="pills-home-tab"
     >
-      <Todo :items="todos" @Delete="DeleteTodo"></Todo>
+      <Todo :items="todos" @delete-todo="DeleteTodo" @update-todo="update"></Todo>
     </div>
   </div>
 </template>
@@ -95,12 +89,28 @@ export default {
   methods: {
     CreateTodo(value) {
      this.todos.push(
-       {key:3, done: true, text:value},
+       {key:Date.now(), done: true, text:value},
      )
     },
      DeleteTodo(index){
        this.todos = this.todos.filter(todo => todo.key != index)
+     },
+
+     update(value, index){
+       console.log(value , index);
+       
+        this.todos = this.todos.map((todo) => {
+         if(todo.key == index){
+           return {
+             ... todo,
+            text :value
+           }
+         }
+            return todo
+        })
+        
      }
+
 
   },
 };
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,38 +1,72 @@
 <template>
+  <div class="mb-2 card" v-for="(todo) in items" :key="todo.key" >
+    <div
+      class="card-body d-flex justify-content-between align-items-center"
+    v-if="edit == false"
+    >
+      {{ todo.text }}
 
-    <div class="card mb-2" v-for="(todo, index) in items" :key="index">
-        <div class="card-body d-flex justify-content-between align-items-center ">
-            {{ todo.text }}
-            <div class="form-group ">
-                <button class="btn btn-primary btn-sm form-contoll">edit</button>
-                 <button class="btn btn-danger btn-sm form-contoll" @click="$emit('delete', todo.key)">Delete</button>
-            </div>
-        </div>
-        
+      <div class="form-group">
+        <button  
+          class="btn btn-primary btn-sm form-contoll"
+          @click="Edit(todo.key)"
+        >
+          edit
+        </button>
+        <button
+          class="btn btn-danger btn-sm form-contoll"
+          @click="$emit('delete-todo', todo.key)"
+        >
+          Delete
+        </button>
+      </div>
     </div>
-
+    <div
+      class="card-body d-flex justify-content-between align-items-center"
+      v-if="edit == true && todo.key == key"
+    >
+      <div class="d-flex form-group">
+        <input class="form-contoll" type="text" v-model="todotext" />
+        <button  
+          class="form-control btn btn-danger btn-sm"
+          @click="update(todotext, key)"
+        >
+          update
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+export default {
+  emits: ["delete-todo", "update-todo"],
 
-    export default { 
-        emits:[
-            'delete'
-        ],
-        props:[
-            'items'
-        ],
+  props: {
+    items: {
+      required: true,
+    },
+  },
 
-        data(){
-            return{ 
+  data() {
+    return {
+      edit: false,
+      key:0,
+      todotext: "",
+    };
+  },
 
-            }
-        },
-
-        methods:[
-
-        ],
-
-    }
-
+  methods: {
+    Edit(key) {
+      this.todotext = this.items.text;
+      this.edit = true;
+      this.key = key;
+    },
+    update(value, index) {
+      this.edit = false;
+      this.$emit("update-todo", this.todotext, index );
+      
+    },
+  },
+};
 </script>
