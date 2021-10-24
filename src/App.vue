b0e72dbb882c762f856c77fc8b7e794b15209675
davood <davoodkhany@gmail.com>
2021-10-24 22:33:17 +0330

    axios get todos methods

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,10 @@
   <div class="mt-5 d-flex justify-content-center">
     <ul class="mb-3 nav nav-pills" id="pills-tab" role="tablist">
       <li class="nav-item" role="presentation">
-        <button
-          class="nav-link active"
-          type="button"
-        >
-          Home
-        </button>
+        <button class="nav-link active" type="button">Home</button>
       </li>
       <li class="nav-item" role="presentation">
-        <button
-          class="nav-link"
-          type="button"
-        >
-          Profile
-        </button>
+        <button class="nav-link" type="button">Profile</button>
       </li>
     </ul>
   </div>
@@ -35,7 +25,8 @@
       flex-column
       justify-content-center
       d-flex
-      align-items-center"
+      align-items-center
+    "
   >
     <div
       class="mb-2 tab-pane fade show active col-4"
@@ -48,7 +39,6 @@
         @delete-todo="DeleteTodo"
         @update-todo="update"
       ></Todo>
-
     </div>
   </div>
 </template>
@@ -70,69 +60,77 @@ export default {
 
   data() {
     return {
-      todos: [
-
-      ],
+      todos: [],
     };
   },
-  created(){
-    axios.get('https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo.json')
-    .then( ({data}) => {
-      let todos = Object.entries(data).map(([key, value]) => {
-        return {
-          key,
-          ... value.todo
-        }
-      })
-      this.todos = todos;
-    })
-    .catch(err => console.log(err))
+  created() {
+    this.getTodos()
   },
 
   methods: {
     CreateTodo(value) {
-      
-      let todo ={
-      done: true, 
-      text:value
-      }
+      let todo = {
+        done: true,
+        text: value,
+      };
 
-      axios.post('https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo.json', {todo})
-      .then(res =>
-
-        this.todos.push({
-          ... todo,
-          key:res.data.name
-        })
-      )
-      .catch(res => console.log(res))
+      axios
+        .post(
+          "https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo.json",
+          { todo }
+        )
+        .then((res) =>
+          this.todos.push({
+            ...todo,
+            key: res.data.name,
+          })
+        )
+        .catch((res) => console.log(res));
     },
 
-     update(value, index){
-    
-      axios.put(`https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo/${index}.json`,{todo:{text:value,done:true}});
-        this.todos = this.todos.map((todo) => {
-         if(todo.key == index){
+    update(value, index) {
+      axios
+        .put(
+          `https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo/${index}.json`,
+          { todo: { text: value, done: true } }
+        )
+        .then(
+          (res) =>
+            (this.todos = this.todos.map((todo) => {
+              if (todo.key == index) {
+                return {
+                  ...todo,
+                  text: value,
+                };
+              }
+              return todo;
+            }))
+        );
+    },
+    DeleteTodo(index) {
+      console.log(index);
+      axios
+        .delete(
+          `https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo/${index}.json`
+        )
+        .then((res) => this.getTodos());
+    },
+    getTodos() {
+          axios
+      .get(
+        "https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo.json"
+      )
+      .then(({ data }) => {
+        let todos = Object.entries(data).map(([key, value]) => {
           return {
-             ... todo,
-            text :value
-           }
-         }
-          return todo
+            key,
+            ...value.todo,
+          };
+        });
+        this.todos = todos;
       })
-      
-     },
-      DeleteTodo(index){
-
-console.log(index);
-      axios.delete(`https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app/todo/${index}.json`)
-      .then(res =>{
-          this.todos = this.todos.filter(todo => todo.key != index)
-      })
-      
-     },
-
-  }
-
-}
+      .catch((err) => console.log(err));
+    }
+  },
+};
 </script>
