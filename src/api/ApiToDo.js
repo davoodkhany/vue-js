6bb63479f2ebf87bc35bbf0694f7da31eb15924f
davood <davoodkhany@gmail.com>
2021-10-25 22:39:02 +0330

    Interceptors axios

--- src/api/ApiToDo.js
+++ src/api/ApiToDo.js
@@ -4,7 +4,26 @@ import axios from 'axios';
 
 const apiAxios = axios.create({
     baseURL: 'https://vuejs-ca936-default-rtdb.europe-west1.firebasedatabase.app',
- 
+    
+})
+
+// apiAxios.interceptors.request.use(function (config) {
+
+//     // console.log(config);
+
+//     return config;
+// },function (err) {
+//     return Promise.reject(err);
+// })
+
+
+apiAxios.interceptors.response.use(function (response) {
+    console.log(response);
+
+    return response;
+
+},function (err) {
+    return promise.reject(err);
 })
 
 export default apiAxios;
